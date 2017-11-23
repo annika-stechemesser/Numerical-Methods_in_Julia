bab3566153d9b5a3991c019be9045766dd5ce4e3
annika-stechemesser <a.stechemesser@warwick.ac.uk>
2017-11-23 18:37:14 +0000

    q3 start

--- MODULES/MyUtils_wb3.jl
+++ MODULES/MyUtils_wb3.jl
@@ -13,3 +13,29 @@ function f_prime_ana(x)
     y=cos(exp(x))*exp(x)
     return y
 end
+
+function golden_section(a,c,f,maxstep)
+    steps=1:1:maxstep
+    estimates=zeros(length(steps))
+    w=(sqrt(5)-1)/2
+    b=a+w*(c-a)
+    for i in steps
+        if abs(c-b)>abs(b-a)
+            x=b+(1-w)*(c-b)
+            if f(b)<f(x)
+                a,b,c=a,b,x
+            else
+                a,b,c=b,x,c
+            end
+        else
+            x=b-(1-w)*(b-a)
+            if f(b)<f(x)
+                a,b,c=x,b,c
+            else
+                a,b,c=a,x,b
+            end
+        end
+        estimates[i]=0.5*(c+a)
+    end
+    return steps,estimates
+end
